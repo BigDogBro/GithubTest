882bdd7b092da57cff58f57dc6b6f920cf7db3a9
wenli <591113521@qq.com>
2016-06-29 15:26:02 +0800

    Commit Test

---
+++ GithubTest/src/com/github/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.github.test;
+
+public class HelloWorld {
+	/**
+	 * @description: 
+	 * @date: 2016年6月29日 下午3:19:32
+	 * @author: wenli
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
